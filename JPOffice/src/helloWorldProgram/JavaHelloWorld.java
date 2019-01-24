e7262db9d1eaf7635748a164fa84330d864d21c4
Avishek Chakraborty <achakraborty83@gmail.com>
2019-01-24 20:35:27 +0530

    comment removed

--- JPOffice/src/helloWorldProgram/JavaHelloWorld.java
+++ JPOffice/src/helloWorldProgram/JavaHelloWorld.java
@@ -3,7 +3,7 @@ package helloWorldProgram;
 public class JavaHelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 	System.out.println("JavaHelloWorld");
 
