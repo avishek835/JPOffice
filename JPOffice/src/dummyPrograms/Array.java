ebb08fb597cd18b8cacc7320ac658ec9013e0f05
Avishek Chakraborty <achakraborty83@gmail.com>
2019-01-24 20:23:27 +0530

    Array class created.

---
+++ JPOffice/src/dummyPrograms/Array.java
@@ -0,0 +1,50 @@
+package dummyPrograms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class Array {
+	static String s;
+	public static void main(String[] args) {
+		String[] a= {"a","b"};
+		 s=a[0];
+		 
+		 System.out.println(s);
+		 s=a[1];
+		 System.out.println(s);
+		 
+		 String[] b=new String[2];
+		 b[0]="c";
+		 b[1]="d";
+		 System.out.println(b.length);
+		 System.out.println(b[0]);
+		 System.out.println(b[1]);
+		 
+		 ArrayList<String> al=new ArrayList<String>();
+		 al.add(0, "1");
+		 al.add(1,"2");
+		 System.out.println(al.get(0));
+		 System.out.println(al.get(1));
+		 
+		 HashMap<String, String> hm=new HashMap<String, String>();
+		 hm.put("2", "1");//not synchronised
+		 hm.put("0", "1");
+		 hm.put("1", "2");
+		 hm.put("1", "3");
+		 System.out.println(hm);
+		 Hashtable ht=new Hashtable();
+		 ht.put("0", "A");//synchronized
+		 ht.put("1", "B");
+		 ht.put("0", "C");
+		 System.out.println(ht);
+		 
+		 
+		 
+		 
+		 
+		 
+
+	}
+
+}
